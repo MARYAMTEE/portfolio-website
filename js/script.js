01ed20de1ef2b811ae24d091c1907edbd43ee130
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-09-29 14:47:48 +0100

    update changes in animation

--- js/script.js
+++ js/script.js
@@ -65,3 +65,20 @@ buttons.forEach(button => {
         showItems(filter);
     });
 });
+
+// animation upon scroll
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+
+            // observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.2});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
